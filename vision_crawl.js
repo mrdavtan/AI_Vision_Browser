003a349e5f5117e79186596b214d9cf2a4b91cd4
mrdavtan <mrdavtan@protonmail.com>
2024-03-04 14:46:30 -0800

    Added functionality to capture entire website as screenshots while scrolling down.

--- vision_crawl.js
+++ vision_crawl.js
@@ -132,7 +132,9 @@ async function scrollOnePageDown(page) {
                 resolve();
             });
         });
+        return true;
     }
+    return false;
 }
 
 async function highlight_links( page ) {
@@ -444,3 +446,14 @@ Please create a list of links for more info`,
         });
     }
 })();
+async function captureEntireWebsite(page) {
+    let screenshotIndex = 0;
+    while (await scrollOnePageDown(page)) {
+        await page.screenshot({
+            path: `screenshot_${screenshotIndex}.jpg`,
+            quality: 100,
+            fullPage: true
+        });
+        screenshotIndex++;
+    }
+}
